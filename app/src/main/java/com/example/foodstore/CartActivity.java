a9351f4043d2b61efcd85b715d7af1b5ad90f332
Joao Neto <jofernandes47@gmail.com>
2022-10-07 16:38:27 -0300

    up

---
+++ app/src/main/java/com/example/foodstore/CartActivity.java
@@ -0,0 +1,32 @@
+package com.example.foodstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.ColorSpace;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.foodstore.databinding.ActivityMainBinding;
+import com.example.foodstore.ui.main.SectionsPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class CartActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private View actionBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/foodstore/MainActivity.java
+++ app/src/main/java/com/example/foodstore/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.foodstore;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-
 import com.example.foodstore.ui.main.SectionsPagerAdapter;
 import com.example.foodstore.databinding.ActivityMainBinding;
 
@@ -33,12 +27,12 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = binding.fab;
+        Intent secondScreen = new Intent(this, CartActivity.class);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(secondScreen);
             }
         });
     }
