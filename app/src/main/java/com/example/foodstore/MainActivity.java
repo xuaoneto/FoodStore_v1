5a68e702e4866e2aeaa98dadee261e3ede6d8136
arnaldoantonio35 <you@example.com>
2022-10-07 20:27:05 -0300

    erro_view

--- app/src/main/java/com/example/foodstore/MainActivity.java
+++ app/src/main/java/com/example/foodstore/MainActivity.java
@@ -2,22 +2,29 @@ package com.example.foodstore;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
 import com.example.foodstore.ui.main.SectionsPagerAdapter;
 import com.example.foodstore.databinding.ActivityMainBinding;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     private ActivityMainBinding binding;
 
@@ -29,25 +36,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
+        ViewPager viewPager;
+        viewPager = binding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = binding.fab;
         Intent secondScreen = new Intent(this, CartActivity.class);
-        URL url = null;
-        try {
-            url = new URL("http://localhost:8080/web/get?key=value");
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        try {
-            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+        FloatingActionButton botaoteste = findViewById(R.id.botaoPteste);
+        botaoteste.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
 
-        }catch(MalformedURLException e){
-
-        }
+            }
+        });
 
 
 
@@ -58,4 +61,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
