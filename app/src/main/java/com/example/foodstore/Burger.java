d1408b0ac8398e2c2d8959972f62b580e9f5cd82
Joao Neto <jofernandes47@gmail.com>
2022-10-07 23:15:52 -0300

    SUCESSO

--- app/src/main/java/com/example/foodstore/Burger.java
+++ app/src/main/java/com/example/foodstore/Burger.java
@@ -6,18 +6,23 @@ import java.lang.String;
 
 public class Burger implements Serializable {
   private String country;
-
   private String img;
-
   private String dsc;
-
   private Integer rate;
-
   private Integer price;
-
   private String name;
-
   private String id;
+
+  public Burger(String id, String name, Integer price, Integer rate, String dsc, String img, String country){
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.rate = rate;
+    this.dsc = dsc;
+    this.img = img;
+    this.country = country;
+  }
+
 
   public String getCountry() {
     return this.country;
--- app/src/main/java/com/example/foodstore/MainActivity.java
+++ app/src/main/java/com/example/foodstore/MainActivity.java
@@ -1,34 +1,49 @@
 package com.example.foodstore;
 
 import android.content.Intent;
+import android.graphics.ColorSpace;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.JsonWriter;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.foodstore.ui.main.SectionsPagerAdapter;
 import com.example.foodstore.databinding.ActivityMainBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ProgressBar loading = findViewById(R.id.loading);
+        loading.setVisibility(View.INVISIBLE);
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
@@ -38,34 +53,46 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = binding.fab;
         Intent secondScreen = new Intent(this, CartActivity.class);
 
-        final TextView textView = (TextView) findViewById(R.id.texto);
-        // ...
+        TextView textView = findViewById(R.id.texto);
 
-        // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://ig-food-menus.herokuapp.com/burgers";
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
-                        textView.setText(response);
-                        // Display the first 500 characters of the response string.
+                    public void onResponse(JSONArray response) {
+                        List<Burger> burgerArray = new ArrayList();
+                        if (response != null) {
+                            for (int i = 0; i < response.length(); i++){
+                                try {
+                                    Burger currentBurger = new Burger(
+                                            response.getJSONObject(i).optString("id"),
+                                            response.getJSONObject(i).optString("name"),
+                                            response.getJSONObject(i).optInt("price"),
+                                            response.getJSONObject(i).optInt("rate"),
+                                            response.getJSONObject(i).optString("dsc"),
+                                            response.getJSONObject(i).optString("img"),
+                                            response.getJSONObject(i).optString("country")
+                                            );
+                                    burgerArray.add(currentBurger);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        Burger burger = burgerArray.get(0);
+                        textView.setText(burger.getCountry()+" "+burger.getName()+" "+burger.getDsc());
+                        loading.setVisibility(View.INVISIBLE);
                     }
+
                 }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    textView.setText("That didn't work!");
-                }
-        });
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-
-
-
-
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("Deu ruim!");
+                        loading.setVisibility(View.INVISIBLE);
+                    }
+                });
 
 
 
@@ -73,11 +100,10 @@ public class MainActivity extends AppCompatActivity {
         botaoteste.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                queue.add(jsonObjectRequest);
+                loading.setVisibility(View.VISIBLE);
             }
         });
-
-
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,7 +112,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
 
 }
