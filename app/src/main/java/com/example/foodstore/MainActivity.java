b9a6a141d9d44ab08c1a9ef13c78c5fedd102a82
Joao Neto <jofernandes47@gmail.com>
2022-10-07 21:15:01 -0300

    GET

--- app/src/main/java/com/example/foodstore/MainActivity.java
+++ app/src/main/java/com/example/foodstore/MainActivity.java
@@ -43,21 +43,21 @@ public class MainActivity extends AppCompatActivity {
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://joaofernandes.ddns.net:9000/";
+        String url ="https://ig-food-menus.herokuapp.com/burgers";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        textView.setText(response);
                         // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
                     }
                 }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText("That didn't work!");
+                }
         });
 
         // Add the request to the RequestQueue.
